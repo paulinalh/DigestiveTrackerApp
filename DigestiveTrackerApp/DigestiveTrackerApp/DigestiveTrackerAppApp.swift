669bf79de9ffc2c7fbab482df6e9072b5c2212fb
paulina <paulinalopezholguin1@gmail.com>
2024-05-13 23:43:52 +0200

    Init xcode project

---
+++ DigestiveTrackerApp/DigestiveTrackerApp/DigestiveTrackerAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  DigestiveTrackerAppApp.swift
+//  DigestiveTrackerApp
+//
+//  Created by Paulina Lopez Holguin on 13/05/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DigestiveTrackerAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
